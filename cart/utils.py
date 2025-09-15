120657c6cc1947f313c85c7ec83438a6529b0351
Gene Chang <108990232+GeneWC@users.noreply.github.com>
2025-09-15 13:16:28 -0400

    fixed price adding bug

--- cart/utils.py
+++ cart/utils.py
@@ -3,4 +3,4 @@ def calculate_cart_total(cart, movies_in_cart):
     for movie in movies_in_cart:
         quantity = cart[str(movie.id)]
         total += movie.price * int(quantity)
-        return total
+    return total
